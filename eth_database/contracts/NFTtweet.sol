e30d8a601799541264a2682c1d7345c7f3ed3e62
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.local>
2021-12-28 13:11:48 +0100

    needs direct + new run.js

---
+++ eth_database/contracts/NFTtweet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract NFTtweet {
+    constructor() {
+        console.log("This is my tweetNFT!");
+    }
+}
---
+++ eth_database/scripts/run-nft.js
@@ -0,0 +1,20 @@
+//Execute: npx hardhat run scripts/run-nft.js
+
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('NFTtweet');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
