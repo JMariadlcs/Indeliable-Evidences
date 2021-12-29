5c21c96a86ad8fcd445f41a1db6e832e611749d0
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.local>
2021-12-29 15:01:59 +0100

    run script update

--- eth_database/scripts/run-nft.js
+++ eth_database/scripts/run-nft.js
@@ -1,18 +1,33 @@
 //Execute: npx hardhat run scripts/run-nft.js
 
+const getCurrentTime = async () => {
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                    + (currentdate.getMonth()+1)  + "/" 
+                    + currentdate.getFullYear() + " @ "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+        
+        return datetime;
+}
+
 const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory('EvidenceNFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
+
+    //Calculation of currentTime
+    var currentTime = (await getCurrentTime()).toString();
   
     // Call the function.
-    let txn = await nftContract.makeNFT()
+    let txn = await nftContract.makeNFT("https://twitter.com/elonmusk/status/1374617643446063105?lang=es", currentTime);
     // Wait for it to be mined.
     await txn.wait()
   
     // Mint another NFT for fun.
-    txn = await nftContract.makeNFT()
+    txn = await nftContract.makeNFT("https://twitter.com/elonmusk/status/1374617643446063105?lang=es", currentTime);
     // Wait for it to be mined.
     await txn.wait()
   
